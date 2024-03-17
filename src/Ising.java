efeb9a18fd6b74d1d67111a2bbb4191ba6048fe7
Alex Erdei <mralexerdei@yahoo.co.uk>
2024-03-17 13:43:24 +0000

    Introduce canvasSize, latticeSize and atomSize vars

--- src/Ising.java
+++ src/Ising.java
@@ -3,8 +3,11 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class Ising extends Canvas implements Runnable {
+  int latticeSize = 20;
+  int canvasSize = 400;
+  int atomSize = canvasSize / latticeSize;
   Ising() {
-    setSize(400, 400);
+    setSize(canvasSize, canvasSize);
     setBackground(Color.WHITE);
     Frame isingFrame = new Frame("Ising Model");
     isingFrame.setLayout(new GridLayout(0, 1));
@@ -24,12 +27,12 @@ public class Ising extends Canvas implements Runnable {
   }
   @Override
   public void paint(Graphics g) {
-    for (int i=0; i<20; i++) {
-      for (int j=0; j<20; j++) {
+    for (int i=0; i<latticeSize; i++) {
+      for (int j=0; j<latticeSize; j++) {
         Color color = Color.blue;
         if ((i + j) % 2==0) color = Color.yellow;
         g.setColor(color);
-        g.fill3DRect(20 * i, 20 * j, 20, 20, false);
+        g.fill3DRect(atomSize * i, atomSize * j, atomSize, atomSize, false);
       }
     }
   }
